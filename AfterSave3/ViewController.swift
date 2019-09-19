a252480c84b055ec88446e68fc4460885e3d3692
juhani-vainio <developer30752@gmail.com>
2019-09-19 12:47:42 +0300

    Initial Commit

---
+++ AfterSave3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AfterSave3
+//
+//  Created by Juhani Vainio on 19/09/2019.
+//  Copyright © 2019 JuhaniVainio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
